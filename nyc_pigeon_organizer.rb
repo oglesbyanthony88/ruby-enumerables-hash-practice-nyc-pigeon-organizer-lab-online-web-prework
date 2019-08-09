f64e9b7da05fc9e1e3b37c6022f7b44d71062a1c
Anthony Oglesby <ogle1ant@gmail.com>
2019-08-09 20:07:15 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new = {}
+  data.each do |key, value|
+    value.each do |new_v, names|
+      names.each do |name|
+        if !new[name]
+          new[name] = {}
+        end
+        if !new[name][key]
+          new[name][key] = []
+        end
+        new[name][key] << new_v.to_s
+      end
+    end
+  end
+  new
 end
