2990515d8d58dee2fda1e7587d117069715a9e9a
Anthony Oglesby <ogle1ant@gmail.com>
2019-08-09 20:03:23 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,17 +3,13 @@ def nyc_pigeon_organizer(data)
   data.each do |key, value|
     value.each do |new_v, names|
       names.each do |name|
-        
         if !new[name]
           new[name] = {}
         end
-        
         if !new[name][key]
           new[name][key] = []
         end
-        
         new[name][key] << new_v.to_s
-        
       end
     end
   end
